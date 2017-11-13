1eafa0b606a8b39efacf5810bbbea91f54aef973
Garcia flores Jorge <garciaflores@lipn-ssh>
2017-11-13 23:58:08 +0100

    init

---
+++ backup_lipnssh.sh
@@ -0,0 +1,1 @@
+scp -r ~/web/garciaflores/* garciaflores@lipnssh.univ-paris13.fr:/users/garciaflores/public_html/bientot
---
+++ backup_main_css.sh
@@ -0,0 +1,1 @@
+scp assets/css/main.css garciaflores@lipnssh.univ-paris13.fr:/users/garciaflores/public_html/bientot/assets/css
